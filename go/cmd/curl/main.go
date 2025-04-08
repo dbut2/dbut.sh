c44ebb7ccd598b6b34f79ba4f1b2de2c4951c647
Dylan Butler <dylan@but.la>
2025-04-08 10:45:55 +0000

    http servers

--- go/cmd/curl/main.go
+++ go/cmd/curl/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"strings"
 
+	"github.com/charmbracelet/glamour"
+
+	"dbut.sh/pages"
+)
+
+func main() {
+	rend, err := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(80),
+		glamour.WithEmoji(),
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix("/", r.URL.Path)
+		if path == "" {
+			path = "index"
+		}
+		bytes, err := pages.MD.ReadFile(path + ".md")
+		if err != nil {
+			panic(err.Error())
+		}
+		bytes, err = rend.RenderBytes(bytes)
+		if err != nil {
+			panic(err.Error())
+		}
+		w.Write(bytes)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go/cmd/web/main.go
+++ go/cmd/web/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"strings"
 
+	"dbut.sh/pages"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix("/", r.URL.Path)
+		if path == "" {
+			path = "index"
+		}
+		bytes, err := pages.HTML.ReadFile(path + ".html")
+		if err != nil {
+			panic(err.Error())
+		}
+		w.Header().Add("Content-Type", "text/html")
+		w.Write(bytes)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
